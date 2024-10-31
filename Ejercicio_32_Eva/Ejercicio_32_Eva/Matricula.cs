0fd4606381ef25ac8f745100ad99c70173c3ce48
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-10-30 21:24:59 -0500

    Creación de clase

--- Ejercicio_32_Eva/Ejercicio_32_Eva/Matricula.cs
+++ Ejercicio_32_Eva/Ejercicio_32_Eva/Matricula.cs
@@ -8,5 +8,24 @@ namespace Ejercicio_32_Eva
 {
     internal class Matricula
     {
+        // Propiedades
+        private int Estudiante { get; set; }
+       
+        public Matricula(int Estudiante)
+        {
+            Estudiante = Estudiante;
+        }
+
+        public void Costo_Subsidio()
+        {
+            decimal Valor_Final = Estudiante.CalculoMatricula();
+            decimal Subsidio = Estudiante.Calculo_Subsidio();
+
+            Console.WriteLine($"El costo de la matricula es: {Valor_Final:C}");
+            Console.WriteLine($"El subsidio otorgado es: {Subsidio:C}");
+        }
+
     }
 }
+
+
